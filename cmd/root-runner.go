7578d64fa7945e4c0fb957fcf1201c2e5351ef07
Rumpu-Jussi <kontza@gmail.com>
2018-09-11 11:46:52 +0300

    Updated parsing logic.

--- cmd/root-runner.go
+++ cmd/root-runner.go
@@ -8,32 +8,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func printParseResult(source string, result time.Time, parser string) {
-	logger.Debug().Str("source", source).Str("result", fmt.Sprintf("%v", result)).Str("parser", parser).Msg("Parsed time value")
-}
-
 func rootRunner(cmd *cobra.Command, args []string) {
 	const timeFormShort = "15:04:05"
-	const timeFormLong = "15:04:05.000"
 	var times [2]time.Time
 	var err error
-	usedLongForm := false
 	for i, arg := range args {
 		arg = strings.Replace(arg, ",", ".", 1)
 		if times[i], err = time.Parse(timeFormShort, arg); err != nil {
-			if times[i], err = time.Parse(timeFormLong, arg); err != nil {
-				logger.Fatal().Str("reason", fmt.Sprintf("%+v", err)).Msg("Parsing failed")
-			} else {
-				printParseResult(arg, times[i], "timeFormLong")
-			}
+			logger.Fatal().Str("reason", fmt.Sprintf("%+v", err)).Msg("Parsing failed")
 		} else {
-			printParseResult(arg, times[i], "timeFormShort")
+			logger.Debug().Str("source", arg).Str("result", fmt.Sprintf("%v", times[i])).Msg("Parsed time value")
 		}
 	}
 	var delta time.Duration
 	var alpha float64
 	var omega float64
-	midnight, _ := time.Parse(timeFormShort, "0:00:00")
+	midnight := time.Date(0, time.January, 1, 0, 0, 0, 0, time.UTC)
 	if times[0].After(times[1]) {
 		delta = times[0].Sub(times[1])
 		alpha = times[1].Sub(midnight).Seconds()
@@ -43,7 +33,10 @@ func rootRunner(cmd *cobra.Command, args []string) {
 		alpha = times[0].Sub(midnight).Seconds()
 		omega = times[1].Sub(midnight).Seconds()
 	}
-	if usedLongForm {
+	truncated := delta.Truncate(time.Second)
+	useFloat := (delta != delta.Truncate(time.Second))
+	logger.Debug().Dur("delta", delta).Dur("truncated", truncated).Bool("use float result", useFloat).Msg("Calculated difference")
+	if useFloat {
 		fmt.Printf("%.3f - %.3f = %.3f\n", omega, alpha, delta.Seconds())
 	} else {
 		fmt.Printf("%d - %d = %d\n", int(omega), int(alpha), int(delta.Seconds()))
